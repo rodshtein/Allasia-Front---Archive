74f1aeef027190747f917714013107ee8d4e32ea
Misha Zamkevich <uwwcom@gmail.com>
2020-09-03 23:50:05 +1000

    Now GA run pull command on server and restart up

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       cwd: "/var/www/front/",
       script: "yarn start",
       instances: 1,
-      watch: true,
+      watch: false,
       watch_delay: 5000,
       ignore_watch : ["node_modules", ".git", "cypress", "src", ".github"],
       watch_options: {
