61edb227a0c2bf933a5b75182b0bfc6535e23751
Misha Zamkevich <uwwcom@gmail.com>
2020-11-04 17:17:15 +1000

    Procedures reordered items

--- src/components/Cards-procedures.svelte
+++ src/components/Cards-procedures.svelte
@@ -42,19 +42,10 @@
             +if('el.feature')
               p.subheader-h3 {el.feature}
 
-          +if('el.duration || el.description')
-            .description-wrap
-              +if('el.duration')
-                .duration
-                  p.p {el.duration}
-              +if('el.description')
-                p.p {el.description}
-
           +if('el.price')
             .price-wrap(class!='{el.price.length < 2 ? "price-wrap--one-item": ""}')
               +each('el.price as el')
                 .price-item
-
                   .first-line-wrap
                     +if('el.country && el.country.name')
                       p.country {el.country.name}
@@ -64,6 +55,15 @@
                   +if('el.conditions')
                     p.conditions {el.conditions}
 
+          +if('el.duration || el.description')
+            .description-wrap(
+              class!='{cls("description-wrap")}'
+            )
+              +if('el.duration')
+                .duration
+                  p.p-note {el.duration}
+              +if('el.description')
+                p.p-note.description {el.description}
 </template>
 
 <style lang='postcss'>
@@ -87,7 +87,6 @@
   &:last-child
     margin-right: 0
 
-
   .header-wrap
     margin-bottom: 15px
     padding-bottom: 15px
@@ -98,26 +97,6 @@
       margin-bottom: 5px
     .subheader-h3
       max-width: 80%
-
-  .description-wrap
-    display: grid
-    grid-auto-flow: row
-    grid-gap: 5px
-    margin-bottom: 15px
-    padding-bottom: 15px
-    @mixin devider_border_bottom
-
-    .duration
-      max-width: 80%
-      p
-        display: flex
-        &:before
-          content: url(/icons/micro/time.svg)
-          display: inline-block
-          margin:
-            top: 1px
-            right: 10px
-
 
   .price-wrap
     display: block
@@ -139,21 +118,47 @@
           line-height: 17px
           margin-bottom: 5px
           white-space: nowrap
+          margin-right: 10px
 
         .price
-          flex-grow: 2
+          flex: 1
           text-align: right
           margin-bottom: 5px
-          white-space: nowrap
 
     .conditions
       font-size: 12px
       font-weight: 400
       line-height: 14px
       text-align: right
-      color: var(--color--p)
+      color: var(--LIGHT-BLUE)
       margin-left: 20%
       white-space: nowrap
+
+  .description-wrap
+    display: grid
+    grid-auto-flow: row
+    grid-gap: 5px
+    margin-top: 10px
+    padding-top: 15px
+    @mixin devider_border_top
+
+    &--1
+      @media(width > 500px)
+        grid-auto-flow: column
+        grid-gap: 50px
+
+    .duration
+      max-width: 80%
+      .p-note
+        display: flex
+        color: var(--LIGHT-BLUE)
+        &:before
+          content: url(/icons/micro/time.svg)
+          display: inline-block
+          margin-right: 10px
+
+    .description
+      color: var(--LIGHT-BLUE)
 
   &--1
     width: 100%
